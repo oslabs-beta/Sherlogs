031c07c81d2b14bda3c0e73849bdbb095a280d17
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-02-14 20:32:04 -0500

    initial setup

---
+++ server/routes/v1.routes/userRouter.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
+const router = express.Router();
+
+router.post('/signup', passport.)
+
+module.exports = router;
