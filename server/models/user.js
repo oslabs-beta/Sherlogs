e5b8c317b0ebd184019d1cf0cf2890c61962ad83
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-02-14 22:39:32 -0500

    configured user router for login and signup

--- server/models/user.js
+++ server/models/user.js
@@ -18,18 +18,29 @@ const userSchema = new Schema({
 })
 
 //validation function (password requirements)
-userSchema.methods.passwordIsCorrectLength = async function(password){
-  const user = this;
-  return password.length >= 8 && password.length <= 16
+//refactor to throw an error instead of returning 
+// userSchema.methods.passwordIsCorrectLength = async function(password){
+//   const user = this;
+//   return password.length >= 8 && password.length <= 16
+// }
+
+// userSchema.methods.usernameIsCorrectLength = async function(username){
+//     const user = this; 
+//     return username.length <= 25
+// }
+
+userSchema.methods.isValidPassword = async function(password) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
 }
 
-//const isValidUsername = 
-
+//refactor: only hashing should be here (line 42), rest should be in controller 
 userSchema.pre(
     'save',
     async function(next){
         const user = this;
-        if(!this.passwordIsCorrectLength(this.password)) return false;
+        // if(!this.passwordIsCorrectLength(this.password)) return false;
         const hash = await bcrypt.hash(this.password, 10);
         this.password = hash;
         next();
--- server/routes/v1.routes/userRouter.js
+++ server/routes/v1.routes/userRouter.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const passport = require('passport');
-const session = require('express-session');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-router.post('/signup', passport.)
+router.post(
+    '/signup',
+    passport.authenticate('signup', { session: false }),
+    async (req, res, next) => {
+      res.json({
+        message: 'Signup successful',
+        user: req.user
+      });
+    }
+  );
+
+  router.post(
+    '/login',
+    async (req, res, next) => {
+      passport.authenticate(
+        'login',
+        async (err, user, info) => {
+          try {
+            if (err || !user) {
+              const error = new Error('An error occurred.');
+  
+              return next(error);
+            }
+  
+            req.login(
+              user,
+              { session: false },
+              async (error) => {
+                if (error) return next(error);
+  
+                const body = { _id: user._id, username: user.username };
+                const token = jwt.sign({ user: body }, 'TOP_SECRET');
+  
+                return res.json({ token });
+              }
+            );
+          } catch (error) {
+            return next(error);
+          }
+        }
+      )(req, res, next);
+    }
+  );
+
 
 module.exports = router;
