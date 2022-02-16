4abc62dbae13a895412e35964c2a8cb55a035220
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-02-15 21:42:07 -0500

    config auth

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -3,50 +3,49 @@ const LocalStrategy = require('passport-local').Strategy;
 const userModel = require('../models/user');
 
 passport.use(
-    'signup',
-    new LocalStrategy(
-      {
-        usernameField: 'username',
-        passwordField: 'password'
-      },
-      async (username, password, done) => {
-        try {
-          const user = await userModel.create({ username, password });
-  
-          return done(null, user);
-        } catch (error) {
-          done(error);
-        }
-      }
-    )
-  );
+  'signup',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      try {
+        const user = await userModel.create({ username, password });
 
-  passport.use(
-    'login',
-    new LocalStrategy(
-      {
-        usernameField: 'username',
-        passwordField: 'password'
-      },
-      async (username, password, done) => {
-        try {
-          const user = await userModel.findOne({ username });
-  
-          if (!user) {
-            return done(null, false, { message: 'User not found' });
-          }
-  
-          const validate = await user.isValidPassword(password);
-  
-          if (!validate) {
-            return done(null, false, { message: 'Wrong Password' });
-          }
-  
-          return done(null, user, { message: 'Logged in Successfully' });
-        } catch (error) {
-          return done(error);
-        }
+        return done(null, user);
+      } catch (error) {
+        done(error);
       }
-    )
-  );
-  
+    }
+  )
+);
+
+passport.use(
+  'login',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      try {
+        const user = await userModel.findOne({ username });
+
+        if (!user) {
+          return done(null, false, { message: 'User not found' });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: 'Wrong Password' });
+        }
+
+        return done(null, user, { message: 'Logged in Successfully' });
+      } catch (error) {
+        return done(error);
+      }
+    }
+  )
+);
--- server/routes/v1.routes/userRouter.js
+++ server/routes/v1.routes/userRouter.js
@@ -4,48 +4,37 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.post(
-    '/signup',
-    passport.authenticate('signup', { session: false }),
-    async (req, res, next) => {
-      res.json({
-        message: 'Signup successful',
-        user: req.user
+  '/signup',
+  passport.authenticate('signup', { session: false }),
+  async (req, res, next) => {
+    res.json({
+      message: 'Signup successful',
+      user: req.user,
+    });
+  }
+);
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('login', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error('An error occurred.');
+
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const body = { _id: user._id, username: user.username };
+        const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+        return res.json({ token });
       });
+    } catch (error) {
+      return next(error);
     }
-  );
-
-  router.post(
-    '/login',
-    async (req, res, next) => {
-      passport.authenticate(
-        'login',
-        async (err, user, info) => {
-          try {
-            if (err || !user) {
-              const error = new Error('An error occurred.');
-  
-              return next(error);
-            }
-  
-            req.login(
-              user,
-              { session: false },
-              async (error) => {
-                if (error) return next(error);
-  
-                const body = { _id: user._id, username: user.username };
-                const token = jwt.sign({ user: body }, 'TOP_SECRET');
-  
-                return res.json({ token });
-              }
-            );
-          } catch (error) {
-            return next(error);
-          }
-        }
-      )(req, res, next);
-    }
-  );
-
+  })(req, res, next);
+});
 
 module.exports = router;
