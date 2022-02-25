64dbfb294f7681068cb2377735b79efd1ece7e70
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-02-24 22:24:09 -0500

    changed model and controller to store app logs
    Co-authored-by: agfeynman@gmail.com

--- server/models/logs.js
+++ server/models/logs.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const logSchema = new Schema({
-  log: {
+  message: {
     type: String,
     required: true,
+  },
+  level: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    required: true,
+    default: Date.now
   },
 });
 
 module.exports = mongoose.model('log', logSchema);
+
