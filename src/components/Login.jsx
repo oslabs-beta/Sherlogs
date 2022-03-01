9accee8823f026e054cbe9ced05647fc088bea4e
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-03-01 10:00:18 -0500

    login layout

---
+++ src/components/Login.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+// import { Link, useNavigate } from 'react-router-dom';
+import { FiUserCheck } from 'react-icons/fi';
+import { Button, TextField } from '@mui/material';
+
+const Login = () => {
+  const [state, setState] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [inputError, setInputError] = useState(false);
+  //   const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:3300/apiv1/auth/login', {
+        username: state.username,
+        password: state.password,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.data.status === 403) {
+          setInputError('Missing username or password.');
+        }
+        if (!res.data.status && res.status === 200) {
+          setInputError('Login Successful');
+          //reroute here using navigate
+        }
+      })
+      .catch((err) => {
+        console.error('err in login request:', err.response);
+        setInputError(true);
+      });
+  };
+
+  return (
+    <div className='flex h-screen bg-white'>
+      <div className='w-full max-w-xs m-auto bg-gray-300 rounded p-5'>
+        <header>
+          <FiUserCheck className='h-10 w-10 text-blue-500 mx-auto mb-5' />
+        </header>
+        <form>
+          <div>
+            <TextField
+              required
+              label='Username'
+              placeholder='Enter username'
+              name='username'
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          <div>
+            <TextField
+              required
+              label='Password'
+              placeholder='Enter password'
+              type='password'
+              name='password'
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          <div>
+            <Button variant='contained' onClick={handleSubmit}>
+              Submit
+            </Button>
+          </div>
+        </form>
+        <footer>
+          <div className='text-center w-full text-red-900 font-bold py-2 px-4 mb-6 rounded'>
+            {inputError && inputError}
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
