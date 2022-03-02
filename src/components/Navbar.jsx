6680aec92d7bd20c3354079a22deeeea3a9fee2f
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-03-01 23:57:23 -0500

    format navbar options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
       <div className='bg-dark-cyan flex justify-between items-center px-4 py-4'>
         <div
           style={{ whiteSpace: 'nowrap' }}
-          className='text-bright-turquoise ml-3 lg:ml-4 bold mt-1 font-semibold text-2xl tracking-tight'>
+          className='text-bright-turquoise ml-3 lg:ml-4 mt-1 font-semibold text-2xl tracking-tight'>
           Sherlogs
         </div>
         <div className='lg:hidden pt-2'>
@@ -31,13 +31,13 @@ const Navbar = () => {
         <a
           href='#'
           style={{ whiteSpace: 'nowrap', fontSize: '18px' }}
-          className='text-bright-turquoise duration-300 block mr-3 ml-3 lg:inline-block  rounded pr-2 pl-2 mt-3 hover:border-b-4 border-tomato pb-1 pt-1'>
+          className='text-bright-turquoise tracking-tight duration-300 block mr-3 ml-3 lg:inline-block rounded pr-2 pl-2 mt-3 hover:border-b-4 border-tomato pb-1 pt-1'>
           Home
         </a>
         <a
           href='#'
           style={{ whiteSpace: 'nowrap', fontSize: '18px' }}
-          className='text-bright-turquoise duration-300 block mr-3 ml-3 lg:inline-block rounded pl-2 pr-2 hover:border-b-4  mt-3 hover:border-b-4 border-tomato pb-1 pt-1'>
+          className='text-bright-turquoise tracking-tight duration-300 block mr-3 ml-3 lg:inline-block rounded pl-2 pr-2 hover:border-b-4  mt-3 hover:border-b-4 border-tomato pb-1 pt-1'>
           Account
         </a>
       </div>
