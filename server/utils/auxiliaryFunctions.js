99114cf09456e5f9aff23ccdac269146399c487b
Kylene Hohman <14060565+velveteen-ant@users.noreply.github.com>
2022-04-04 20:21:38 -0400

    refactor aux function

--- server/utils/auxiliaryFunctions.js
+++ server/utils/auxiliaryFunctions.js
@@ -1,5 +1,5 @@
-module.exports = {
-  checkLogBody(body) {
-    return Array.isArray(body.logs) ? body.logs[0] : body;
-  },
+const checkLogBody = (body) => {
+  return Array.isArray(body.logs) ? body.logs[0] : body;
 };
+
+module.exports = { checkLogBody };
